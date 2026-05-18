219139a1ea8afd2cdfbd45f8c8237360e8bd1c07
yuzheng <yuzheng@tmrtek.com>
2026-05-18 11:34:50 +0800

    fix

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -27,20 +27,22 @@ export default function Home() {
       <div className="homeBox">
         <div className='homeBoxInput'>
           <div className='homeBoxInputTitle'>請輸入Steam遊戲ID或是網址</div>
-          <input className='homeBoxInputBox' type='text' value={form.id} onChange={(e)=>setForm({...form,id:e.target.value})} />
-          <select className='homeBoxInputSelect' value={form.model} onChange={(e)=>setForm({...form,model:e.target.value})}>
-            <option value=''>請選擇模型</option>
-            {model.map(r=> <option value={r.name} key={r.name}>{r.name}</option>)}
-          </select>
-          <select className='homeBoxInputSelect' value={form.size} onChange={(e)=>setForm({...form,size:e.target.value})}>
-            <option value=''>請選擇評論數</option>
-            <option value={100}>前100個評論</option>
-            <option value={300}>前300個評論</option>
-            <option value={500}>前500個評論</option>
-            <option value={800}>前800個評論</option>
-            <option value={1000}>前1000個評論</option>
-          </select>
-          <button className='homeBoxInputButton' onClick={genReport}>Generate</button>
+          <div className='d-flex align-items-center justify-content-cente mb-4'>
+            <input className='homeBoxInputBox' type='text' value={form.id} onChange={(e)=>setForm({...form,id:e.target.value})} />
+            <select className='homeBoxInputSelect' value={form.model} onChange={(e)=>setForm({...form,model:e.target.value})}>
+              <option value=''>請選擇模型</option>
+              {model.map(r=> <option value={r.name} key={r.name}>{r.name}</option>)}
+            </select>
+            <select className='homeBoxInputSelect' value={form.size} onChange={(e)=>setForm({...form,size:e.target.value})}>
+              <option value=''>請選擇評論數</option>
+              <option value={100}>前100個評論</option>
+              <option value={300}>前300個評論</option>
+              <option value={500}>前500個評論</option>
+              <option value={800}>前800個評論</option>
+              <option value={1000}>前1000個評論</option>
+            </select>
+          </div>
+          <button className='homeBoxInputButton btn btn-primary btn-lg' onClick={genReport}>Generate</button>
         </div>
       </div>
     </div>
