40dd476f1433b3e5463978d733193e0563f2868e
iga high <iga33087@gmail.com>
2026-05-31 18:09:52 +0800

    fix

--- src/components/PageBox.tsx
+++ src/components/PageBox.tsx
@@ -1,10 +1,21 @@
 export default function PageBox({total,data,setPageForm}) {
   function pageList() {
-    let res=[]
-    let num=Math.ceil(total/data.limit)
-    for(let i=1;i<=num;i++) {
-      res.push(<div className={`pageBoxInnerItem ${i===data.page&&'pageBoxInnerItemOn'}`} key={i} onClick={()=>setPageForm({...data,page:i})}>{i}</div>)
+    let eleArr=[]
+    let maxNum=Math.ceil(total/data.limit)
+    let pageArr=[]
+    for(let i=data.page-3;i<=data.page+3;i++) {
+      if(i>0&&i<=maxNum) pageArr.push(i)
     }
+    for(let item of pageArr) {
+      eleArr.push(<div className={`pageBoxInnerItem ${item===data.page&&'pageBoxInnerItemOn'}`} key={item} onClick={()=>setPageForm({...data,page:item})}>{item}</div>)
+    }
+    let res=<>
+      <div className="pageBoxInnerItem" onClick={()=>setPageForm({...data,page:1})}>＜＜</div>
+      <div className="pageBoxInnerItem" onClick={()=>setPageForm({...data,page:data.page-1})}>＜</div>
+      {eleArr}
+      <div className="pageBoxInnerItem" onClick={()=>setPageForm({...data,page:data.page+1})}>＞</div>
+      <div className="pageBoxInnerItem" onClick={()=>setPageForm({...data,page:maxNum})}>＞＞</div>
+    </>
     return res
   }
 
